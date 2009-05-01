e1df492594fe9d8c991a8ddbc71fbdf9964c28df
Rodney Peck <rodneyp@yahoo-inc.com>
2009-04-30 17:26:17 -0700

    first testing

---
+++ range.rb
@@ -0,0 +1,8 @@
+class Range
+  def initialize(arg)
+    @d = arg
+  end
+  def to_s
+    @d.to_s
+  end
+end
---
+++ test/range_test.rb
@@ -0,0 +1,21 @@
+
+require 'range'
+require 'test/unit'
+
+class RangeTest < Test::Unit::TestCase
+  def test_range
+    [["", []],
+     ["1", [1]],
+     ["1-2", [1,2]],
+     ["1-3", [1,2,3]],
+     ["1-4", [1,2,3,4]],
+     ["1-3,5", [1,2,3,5]],
+     ["1-3,5-7", [1,2,3,5,6,7]],
+     ["1,3-5", [1,3,4,5]],
+     ["1,3,5,7,9", [1,3,5,7,9]],
+     ["1,3,5-7,9", [1,3,5,6,7,9]]].each do |s,a|
+       r = Range.new(a)
+       assert_equal s, r.to_s
+    end
+  end
+end
