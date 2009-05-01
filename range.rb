a0bcb69572fe31e0b441737c2c97a741440e0c5d
Rodney Peck <rodneyp@yahoo-inc.com>
2009-04-30 20:17:30 -0700

    working with all unit tests for range class

--- range.rb
+++ range.rb
@@ -3,12 +3,32 @@ class Range
     @d = []
     @start = nil
     @last = nil
+    @next = nil
     arg.sort.each {|x| add(x)}
   end
   def add(i)
-    @start = i
+    if @start.nil?
+      @start = i 
+    elsif i != @next
+      @d << range_part_str
+      @start = i
+    end
+    @last = i
+    @next = i + 1
   end
+  def range_part_str
+    if @start == @last
+      @start.to_i
+    else
+      "#{@start}-#{@last}"    
+    end
+  end  
   def to_s
-    @start.to_s
+    if @start == @last and @d.length == 0
+      @start.to_s
+    else
+      @d << range_part_str
+      @d.join(",")
+    end
   end
 end
