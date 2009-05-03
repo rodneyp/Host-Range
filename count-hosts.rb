8752b5aece273d885f78ddc272c2de98f463bf38
Rodney Peck <rodneyp@yahoo-inc.com>
2009-05-02 20:57:08 -0700

    use hostrange subclass

--- count-hosts.rb
+++ count-hosts.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/ruby
 require 'range'
 
+class HostRange < Range
+  def initialize(host)
+    super([])
+    parts = host.split(/\./)
+    if /(.*)\d+$/.match(parts.first)
+      @name = $1
+    else
+      @name = parts.first
+    end
+    @host = parts[1..-1].join(".")
+  end
+  def to_s
+    "#{@name}#{super}.#{@host}"
+  end
+end
+
 colo = Hash.new{nil}
 
 while not $stdin.eof? do 
@@ -15,19 +31,18 @@ while not $stdin.eof? do
     n = $2.reverse
     d = $1.reverse.to_i
     if colo[coloname][n].nil?
-      colo[coloname][n] = Range.new([])
+      colo[coloname][n] = HostRange.new(h)
     end
     colo[coloname][n].add(d)
   else
-    colo[coloname][p.last] = Range.new([]) if colo[coloname][p.last].nil?
+    colo[coloname][p.last] = HostRange.new(h) if colo[coloname][p.last].nil?
   end
-  
 end
 
 def show_hosts(hosts,colo)
   hosts.keys.sort.each do |k|
   #  puts "  #{k}#{hosts[k].summary}"
-    puts "#{k}#{hosts[k].to_s}.grp.#{colo}"
+    puts hosts[k].to_s
   end
 end
 
